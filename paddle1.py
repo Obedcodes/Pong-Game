0f5c95c37608e934bd633bb9960374f42f58783f
obed <aguochaobed1@gmail.com>
2024-06-24 01:48:02 +0100

    Pong Game

---
+++ paddle1.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+
+class Paddle:
+
+    def Paddle1(self):
+        paddle_1 = Turtle()
+        paddle_1.shape("square")
+        paddle_1.color("white")
+        paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+        #paddle_1.setpos(x_pos= 350, x_pos= 0)
+
+
